42343300d6f6287d3a9e826819963a3ed3befe34
kang9kang9 <kang9kang9@deephigh.net>
2022-02-16 15:05:15 +0900

    간단 수정작업

--- src/layouts/header.vue
+++ src/layouts/header.vue
@@ -2,21 +2,25 @@
   <div>
     <b-card title="Card Title" no-body>
       <b-card-header header-tag="nav">
-        <b-nav pills align="center" >
-          <b-nav-item to='/home'  active-class='active'>마이홈</b-nav-item>
-          <b-nav-item to='/image' active-class='active'>이미지</b-nav-item>
-          <b-nav-item to='/list' active-class='active'>게시판</b-nav-item>
-        </b-nav>
+        <b-button v-b-toggle.collapse-1 class="menubtn" variant="primary" >메뉴</b-button> 
+        <b-collapse id="collapse-1" class="mt-1">
+          <b-nav vertical >
+            <b-nav-item to='/home'  active-class='active'>마이홈</b-nav-item>
+            <b-nav-item to='/image' active-class='active'>이미지</b-nav-item>
+            <b-nav-item to='/list' active-class='active'>게시판</b-nav-item>
+          </b-nav>
+        </b-collapse>
       </b-card-header>
     </b-card>
   </div>
 </template>
 
 <script>
+
 export default {
 }
 </script>
 
 <style>
-
+.menubtn{float: right; }
 </style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="home">
-    <h2>여기는 홈 밑의 홈 입니다</h2> 
+  <div class="home"> 
+    <h2>home</h2>
   </div>
 </template>
 
